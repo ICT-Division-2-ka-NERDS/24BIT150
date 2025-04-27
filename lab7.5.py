e45b200b926bd52433d115f3ae00e194d3f104cf
drashti patel <24bit150@sot.pdpu.ac.in>
2025-04-27 15:59:13 +0530

    lab7.5.py

---
+++ lab7.5.py
@@ -0,0 +1,17 @@
+dict1={"a": 5,
+       "b": 10,
+       "c": 20,
+       }
+dict2={"a":2,
+       "b":3,
+       "c":4,
+       }
+a=dict1["a"]
+b=dict1["b"]
+c=dict1["c"]
+
+a1=dict2["a"]
+b1=dict2["b"]
+c1=dict2["c"]
+total=a*a1+b*b1+c*c1
+print(total)
