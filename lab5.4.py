a9abd8fb6fce8f6ad538ebd56a52d91ad1b5c0f4
drashti patel <24bit150@sot.pdpu.ac.in>
2025-04-27 10:47:57 +0530

    lab5.4.py

---
+++ lab5.4.py
@@ -0,0 +1,12 @@
+import random
+lst=[]
+for i in range(30):
+    num=random.randint(1,30)
+    lst.append(num)
+    print(lst)
+
+lst2=[]
+for i in range(30):
+    num=random.randint(-100,-1)
+    lst2.append(num)
+    print(lst2)
