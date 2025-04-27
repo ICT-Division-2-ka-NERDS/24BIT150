1222bcd7b1f79919b6f08aeb5ed2d4a8872a904f
drashti patel <24bit150@sot.pdpu.ac.in>
2025-04-27 10:52:15 +0530

    lab5.5.py

---
+++ lab5.5.py
@@ -0,0 +1,7 @@
+a=input("enter a string:")
+b=input("enter a string:")
+c=input("enter a string:")
+d=input("enter a string:")
+e=input("enter a string:")
+lst=[a.upper(),b.upper(),c.upper(),d.upper(),e.upper()]
+print(lst)
