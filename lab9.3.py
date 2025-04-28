fbdade7ee59a2493313a89e7398f704c13d00e76
drashti patel <24bit150@sot.pdpu.ac.in>
2025-04-28 15:31:16 +0530

    lab9.3.py

--- lab9.3.py
+++ lab9.3.py
@@ -1,2 +1,22 @@
+def create_array(a,b,c,n):
+    array=[]
+    
+    for i in range(a):
+        layer=[]
 
+        for j in range(b):
+            row=[]
+            
+            for k in range(c):
+                row.append(n)
+                
+            layer.append(row)
+
+
+        array.append(layer)
+    print(array )
+      
+    return array
+
+create_array(3,4,5,7)
     
